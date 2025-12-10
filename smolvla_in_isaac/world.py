2195c6da454cfa2a28561c965bf5fe2263eea361
may33 <eternalmay33@gmail.com>
2025-12-10 21:27:40 +0100

    start onm the teleop

--- smolvla_in_isaac/world.py
+++ smolvla_in_isaac/world.py
@@ -6,11 +6,20 @@ from isaaclab.app import AppLauncher
 app_launcher = AppLauncher()
 sim_app = app_launcher.app
 
+
+# from smolvla_in_isaac.robots.so_101_cfg import so101_cfg
+
+
+from isaaclab.assets import ArticulationCfg
 from isaaclab.sim import SimulationContext, SimulationCfg
 import isaaclab.sim as sim_utils
 from isaaclab.assets import ArticulationCfg, Articulation
 from isaaclab.actuators import ImplicitActuatorCfg
+
 from isaaclab.devices import Se2Keyboard, Se2KeyboardCfg
+
+
+from lerobot.teleoperators.so101_leader import SO101Leader, SO101LeaderConfig
 
 sim_cfg = SimulationCfg(dt=0.01)
 sim = SimulationContext(sim_cfg)
@@ -20,9 +29,8 @@ sim.set_camera_view((2.5, 2.5, 2.5), (0.0, 0.0, 0.0))
 ground = sim_utils.GroundPlaneCfg()
 ground.func("/World/GroundPlane", ground)
 
-# joints: ['shoulder_pan', 'shoulder_lift', 'elbow_flex', 'wrist_flex', 'wrist_roll', 'gripper']
 
-robot_cfg = ArticulationCfg(
+so101_cfg = ArticulationCfg(
     prim_path = "/World/Robot",
     spawn = sim_utils.UsdFileCfg(
         usd_path = "/home/may33/projects/ml_portfolio/robotics/robots/SO-ARM100/convex_fripper_robot.usd",
@@ -54,6 +62,8 @@ robot_cfg = ArticulationCfg(
     }
 )
 
+robot_cfg = so101_cfg
+
 robot = Articulation(robot_cfg)
 
 sim.reset()
@@ -70,23 +80,28 @@ keyboard = Se2Keyboard(Se2KeyboardCfg())
 
 current_positions = robot.data.default_joint_pos.clone()
 
-print("Controls:")
-print("  W/S - shoulder_lift up/down")
-print("  A/D - shoulder_pan left/right")
-print("  Q/E - elbow_flex")
-print("  ESC - quit")
+
+leader_config = SO101LeaderConfig(
+    port="/dev/ttyACM0",
+    id="blue"
+)
+
+leader = SO101Leader(leader_config)
+
+leader.connect()
 
 while sim_app.is_running():
-    # Get keyboard input - returns (delta, command)
     delta = keyboard.advance()
+
+    action = leader.get_action()
+
+    print("Action:", action)
 
     # print("delta:", delta)
 
     # delta is tuple (x, y, yaw)
-    # WASD controls
-    current_positions[0, 0] += delta[1] * 0.05  # shoulder_pan (W/S)
-    current_positions[0, 1] += delta[0] * 0.05  # shoulder_lift (A/D)
-    current_positions[0, 2] += delta[2] * 0.05  # elbow_flex (Q/E for yaw)
+    current_positions[0, 0] -= delta[1] * 0.05
+    current_positions[0, 1] += delta[0] * 0.05
 
     # Apply positions to robot
     robot.set_joint_position_target(current_positions)
